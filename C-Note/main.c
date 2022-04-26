4b2cb8743c69cf5ba2c023ac3178f0e3e167aba0
Liu Hua <a224412142009@hotmail.com>
2022-04-26 23:46:28 +0800

    Initial Commit

---
+++ C-Note/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C-Note
+//
+//  Created by 劉晉華 on 2022/4/26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
